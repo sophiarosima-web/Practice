2fe50193f891b80d8b51b9c7171714410e805059
sophiarosima-web <sophiarosima@gmail.com>
2026-09-20 14:43:59 +0800

    adding some information on header

---
+++ Demo/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Food Order System</h1>
+      <p>Order your favorite food easily!</p>
+    </header>
+  );
+}
+
+export default Header;
